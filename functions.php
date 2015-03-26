393641fdb75f8b4276ff2f5386dff334d2977025
Stephanie Yen <sdeeyen@gmail.com>
2015-03-25 20:25:20 -0700

    implemented fonts

--- functions.php
+++ functions.php
@@ -97,7 +97,9 @@ add_filter( 'walker_nav_menu_start_el', 'mod_nav_description', 10, 4 );
  */
 function mod_scripts() {
 	wp_enqueue_style( 'mod-style', get_stylesheet_uri() );
-
+    
+    wp_enqueue_style( 'mod-google-fonts', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic|Roboto:500,500italic');
+    
 	wp_enqueue_script( 'mod-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'mod-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
